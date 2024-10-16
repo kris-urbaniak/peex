3ee1cb7890830265637173ece597601010483971
Krzysztof Urbaniak <krzysztof.urbaniak@sainsburys.co.uk>
2024-10-16 13:41:33 +0200

    test table creation

--- sql/tables/USERS/20241016_create_table.sql
+++ sql/tables/USERS/20241016_create_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,                   -- Auto-incrementing primary key
-    username VARCHAR(50) NOT NULL UNIQUE,    -- Unique username, must not be null
+    uname VARCHAR(50) NOT NULL UNIQUE,    -- Unique username, must not be null
     email VARCHAR(100) NOT NULL UNIQUE,      -- Unique email, must not be null
     password_hash VARCHAR(255) NOT NULL,     -- Password hash for user authentication
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Record creation time
