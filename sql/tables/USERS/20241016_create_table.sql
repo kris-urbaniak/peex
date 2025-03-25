5a8716ceeda55977ab279daf181e97b92b398889
Krzysztof Urbaniak <krzysztof.urbaniak@sainsburys.co.uk>
2025-03-25 22:15:51 +0100

    dummy commit

--- sql/tables/USERS/20241016_create_table.sql
+++ sql/tables/USERS/20241016_create_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,                   -- Auto-incrementing primary key
+    id SERIAL PRIMARY KEY,                 -- Auto-incrementing primary key
     uname VARCHAR(50) NOT NULL UNIQUE,    -- Unique username, must not be null
     email VARCHAR(100) NOT NULL UNIQUE,      -- Unique email, must not be null
     password_hash VARCHAR(255) NOT NULL,     -- Password hash for user authentication
