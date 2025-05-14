b6250744c497e2637fbf789fffe45f5af2063194
kris-urbaniak <kris.urbaniak@gmail.com>
2025-05-14 19:57:14 +0200

    table adj

--- sql/tables/DIM_ORBIT/20250503_create_table.sql
+++ sql/tables/DIM_ORBIT/20250503_create_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS warehouse.dim_orbit
 (
-    orbit_id integer,
+    orbit_id integer NOT NULL,
     orbit_name character varying(50),
     orbit_abbreviation character varying(20),
-    load_ts timestamp without time zone
+    load_ts timestamp without time zone,
+    PRIMARY KEY (orbit_id)
 );
